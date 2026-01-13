505b8ff3b35ddf5792d024680a8e9082b62fcb93
reDNIkedia <jnukin01@gmail.com>
2026-01-13 14:35:02 +0530

    setup done

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE_NAME = f"log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+LOG_DIR = "logs"
+logs_path = os.path.join(os.getcwd(), LOG_DIR,LOG_FILE_NAME)
+
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE_NAME)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    filemode='w',
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
+
+if __name__ == "__main__":
+    logging.info("Logging started")
+    logging.info("Logging ended")
